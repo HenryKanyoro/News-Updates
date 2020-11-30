7039b27840ad5e44f28ed74077b1762ee1ffce03
HenryKanyoro <kanyorohenry6@gmail.com>
2020-11-30 20:05:00 +0300

    changes on config

--- config.py
+++ config.py
@@ -1,38 +1,38 @@
-import os
+# import os
 
 
-class Config:
-    '''
-    General configuration parent class
-    '''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
+# class Config:
+#     '''
+#     General configuration parent class
+#     '''
+#     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
 
-    ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+#     ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
 
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-
-
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
+#     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
-class DevConfig(Config):
-    '''
-    Development  configuration child class
+# class ProdConfig(Config):
+#     '''
+#     Production  configuration child class
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+#     pass
 
-    DEBUG = True
 
-config_options = {
-'development': DevConfig,
-'production': ProdConfig
-}
+# class DevConfig(Config):
+#     '''
+#     Development  configuration child class
+
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+
+#     DEBUG = True
+
+# config_options = {
+# 'development': DevConfig,
+# 'production': ProdConfig
+# }
